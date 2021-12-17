d26fbcd4e2e02046bde2918af178b273874a9db4
Laura van den Dikkenberg <daxterr1@hotmail.com>
2021-12-17 13:20:03 +0100

    Updated steam project

--- Project_Steam/main.py
+++ Project_Steam/main.py
@@ -1,20 +1,25 @@
 from tkinter import *
+import json
 from tkinter.messagebox import showinfo
 
 def hoofdscherm():
     root = Tk()
+    root.title('Steam Dashboard')
 
-    label = Label(master=root, text='STEAM Dashboard Groep 1', height=2)
+    label = Label(master=root, text='STEAM Groep 1', height=2)
     label.pack(fill="both", expand=True)
 
-    button1 = Button(master=root, text='Login', command=login)
-    button1.pack(pady=10)
+    login_button = Button(master=root, text='Login', command=login)
+    login_button.pack(pady=10)
 
-    button2 = Button(master=root, text='Games', command=games)
-    button2.pack(pady=10)
+    game_button = Button(master=root, text='Games', command=games)
+    game_button.pack(pady=10)
 
-    button3 = Button(master=root, text='Exit', command=exit)
-    button3.pack(pady=10)
+    lijstspel_button = Button(master=root, text='lijst', command=newwindow)
+    lijstspel_button.pack(pady=10)
+
+    exit_button = Button(master=root, text='Exit', command=root.destroy)
+    exit_button.pack(pady=10)
     #entry = Entry(master=root)
     #entry.pack(padx=10, pady=10)
 
@@ -27,10 +32,32 @@ def login():
 
 def games():
     print("test games")
-    return
+    # Load Json file
+    steam_files = open('steam.json')
+    data = json.load(steam_files)
+    list_of_game_names = []
 
-def exit():
-    print("test logout")
+    # Get every item in data
+    for i in data:
+        list_of_game_names.append(i['name'])
+
+    return newwindow(list_of_game_names)
+
+
+def newwindow(game_names):
+    # Sort the list of game names
+    game_names.sort()
+
+
+    root = Tk()
+    root.title('Steam gamelist')
+
+    scroll = Scrollbar(master=root)
+    scroll.pack(side = RIGHT)
+
+    game_name_label = Label(master=root, text=game_names, height=2)
+    game_name_label.pack(fill="both", expand=True)
+
     return
 
 hoofdscherm()
