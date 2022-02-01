48e7db597fb77d820d7e41c156d9eeae64e353d5
serca7 <serhat.caliskan@student.hu.nl>
2022-02-01 19:30:01 +0100

    Update custom.py

--- Project_Steam/custom.py
+++ Project_Steam/custom.py
@@ -4,6 +4,7 @@ from time import *
 import RPi.GPIO as GPIO
 import time
 
+
 GPIO.setmode( GPIO.BCM )
 GPIO.setwarnings(False)
 
@@ -16,25 +17,48 @@ tm = tm1637.TM1637(21,20, brightness=1)
 GPIO.setup( switch1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN )
 GPIO.setup( switch2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN )
 GPIO.setup( switch3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN )
-        
+
+
+
+def start():
+    file = open('dates.txt', 'r')
+    lines = file.readlines()
+    word_list = []
+
+    for i in lines:
+        word_list.append(i.split(' '))
+        print(word_list)
+        global date
+        global amount_of_players
+        global play_time
+        global game_name
+        date = i.split(' ')[0].replace('/','-')
+        amount_of_players = i.split(' ')[2]
+        #play_time = i.split(' ')[4]
+        play_time = i.split(' ')[4].replace(':','-')
+        game_name = i.split(' ')[6].replace(' ','-')
+        print(f'd: {date} p: {amount_of_players} t: {play_time} g: {game_name}')
+        i.strip()
+
+        file.close()
+        data()
+
 def data():
-    while(True):
+     while(True):
         #Gamename scroll
         if(GPIO.input( switch1)):
-            tm.scroll('Gamenaam')          
-            continue
+            tm.scroll(game_name)
         
         #Datum scroll
         elif(GPIO.input(switch2)):
-            tm.scroll('datum')
-            continue
-        
+            tm.scroll(date)
+            tm.scroll(play_time)
+
         #Spelers scroll
         elif(GPIO.input(switch3)):
-            tm.scroll('Spelers')
-            continue
+            tm.scroll('Amount of players ' + amount_of_players)
 
-Spelers = 'shika'
+start()
 
-data()
- 
+print(date)
+
